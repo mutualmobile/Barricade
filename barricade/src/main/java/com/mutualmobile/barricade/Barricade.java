0cd9cecba3a76c174c8692eb3c9b05c643dd3f54
Suchandrim Sarkar <suchandrim.sarkar@gmail.com>
2017-04-10 12:32:19 +0530

    1. Added Reset Button in Barricade Activity 2. Added Delay reset in Barricade

--- barricade/src/main/java/com/mutualmobile/barricade/Barricade.java
+++ barricade/src/main/java/com/mutualmobile/barricade/Barricade.java
@@ -188,5 +188,6 @@ public class Barricade {
       BarricadeResponseSet set = configs.get(key);
       set.defaultIndex = set.originalDefaultIndex;
     }
+    this.delay = DEFAULT_DELAY;
   }
 }
--- barricade/src/main/java/com/mutualmobile/barricade/activity/BarricadeActivity.java
+++ barricade/src/main/java/com/mutualmobile/barricade/activity/BarricadeActivity.java
@@ -1,8 +1,10 @@
 package com.mutualmobile.barricade.activity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -107,6 +109,9 @@ public class BarricadeActivity extends AppCompatActivity
     } else if (itemId == R.id.menu_delay) {
       showEditDialog();
       return true;
+    } else if (itemId == R.id.menu_reset) {
+      showResetDialog();
+      return true;
     }
     return super.onOptionsItemSelected(item);
   }
@@ -115,5 +120,20 @@ public class BarricadeActivity extends AppCompatActivity
     FragmentManager fm = getSupportFragmentManager();
     EditGlobalDelayDialogFragment dialogFragment = new EditGlobalDelayDialogFragment();
     dialogFragment.show(fm, EditGlobalDelayDialogFragment.class.getName());
+  }
+
+  private void showResetDialog() {
+    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    builder.setMessage(getString(R.string.reset_message))
+        .setCancelable(true)
+        .setNeutralButton(R.string.ok, new DialogInterface.OnClickListener() {
+          @Override public void onClick(DialogInterface dialogInterface, int i) {
+            Barricade.getInstance().reset();
+            endpointsRVAdapter.notifyDataSetChanged();
+          }
+        })
+        .create();
+    AlertDialog alert = builder.create();
+    alert.show();
   }
 }
