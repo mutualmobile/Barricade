d5d795755f8229ca27dfc82d119b6b09279e2d0d
Anmol Verma <anmol.verma4@gmail.com>
2021-11-17 21:09:59 +0530

    1. Updated Maven-publish 2. Updated libraries. 3. WIP for migrating to kotlin-poet 4. Look into KSP ?

--- app/src/main/java/com/mutualmobile/barricade/sample/api/ChuckNorrisApiService.java
+++ app/src/main/java/com/mutualmobile/barricade/sample/api/ChuckNorrisApiService.java
@@ -5,15 +5,19 @@ import com.mutualmobile.barricade.annotation.Response;
 import com.mutualmobile.barricade.sample.api.model.Joke;
 
 import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface ChuckNorrisApiService {
 
-  @GET("/jokes/random") @Barricade(endpoint = "random", responses = {
-      @Response(fileName = "success.json",isDefault = true),
-      @Response(fileName = "failure.json", statusCode = 401)
-  }) Call<Joke> getRandomJoke();
+    @GET("/jokes/random")
+    @Barricade(endpoint = "random", responses = {
+            @Response(fileName = "success.json", isDefault = true),
+            @Response(fileName = "failure.json", statusCode = 401)
+    })
+    Call<Joke> getRandomJoke();
 
-  @GET("/jokes/categories") Call<List<String>> getJokeCategories();
+    @GET("/jokes/categories")
+    Call<List<String>> getJokeCategories();
 }
